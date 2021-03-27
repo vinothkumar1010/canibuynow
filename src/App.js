319e6ad8ba102824ebf2f8d4ff72d5e586ee09de
Kumar <VKumar2@nrg.com>
2021-03-27 17:29:08 -0500

    updated UI and fixed functionality issues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import {useReducer,useEffect} from "react"
 import Decision from "./Decision"
+import "./App.css"
+import "./component.css"
+import Tooltip from "./components/Tootip/index"
+
+
 const initialState = {
   fcfEachyear: [],
+  pvEachYear:[],
   tenthYearFCF: 0,
   futureCashFlow: null,
-  marketCapCal:0
+  marketCapCal:0,
+  showDecision:false
 };
 
 const reducer = (state, {type,value}) => {
@@ -18,15 +25,23 @@ const reducer = (state, {type,value}) => {
       return  {...state, tenthYearFCF:value };
     case "updateFutureCashFlow":
       return  {...state, futureCashFlow:value };
+    case "updatePresentValue":
+      return  {...state, pvEachYear:value };
+    case "updateTenthYearPresentValue":
+      return  {...state, tenthYearPV:value };
     case "updateMarketCap":
       return  {...state, marketCapCal:value };
+    case "showHideResult":
+      return  {...state, showDecision:value };
+    case "setDiscountRate":
+      return  {...state, selectedDiscountRate:value };
     default:
         return state;  
   }
 };
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { fcfEachyear, tenthYearFCF, futureCashFlow,marketCapCal} = state;
+  const { fcfEachyear,selectedDiscountRate, pvEachYear,tenthYearFCF, tenthYearPV,futureCashFlow,marketCapCal,showDecision} = state;
   function updateSelectedValue(elem)
   {
       document.getElementById((elem.id)+"Selected").innerHTML=elem.value;
@@ -54,13 +69,13 @@ function App() {
     for(let i=2;i<=5;i++)
     {
       nextCashFlow=nextCashFlow+(nextCashFlow*firstFiveGR)
-      console.log(Math.round(nextCashFlow)+ "   " + nextCashFlow)
+     // console.log(Math.round(nextCashFlow)+ "   " + nextCashFlow)
       fcfYearBreakDown.push(nextCashFlow)
     }
     for(let i=6;i<=10;i++)
     {
       nextCashFlow=nextCashFlow+(nextCashFlow*nextFiveYearGR)
-      console.log(Math.round(nextCashFlow)+ "   " + nextCashFlow)
+     // console.log(Math.round(nextCashFlow)+ "   " + nextCashFlow)
       fcfYearBreakDown.push(nextCashFlow)
     }
     let TenthYearFCF=nextCashFlow*terminalValue;
@@ -80,30 +95,51 @@ function App() {
   }
   useEffect(() => {
     let discountRate=parseInt(document.getElementById("discountValue").value)/100;
-
+    dispatch(
+      {
+        type:"setDiscountRate",
+        value:discountRate
+      });
     if(fcfEachyear.length>0)
     calculatePresentValue(discountRate)
 }, [fcfEachyear])
 useEffect(()=>{
   let excessCapital=parseFloat(document.getElementById("excessCapital").value);
   if(futureCashFlow !== null)
-    calculateIntrinsicValue(excessCapital).then(console.log("hai"));
+    calculateIntrinsicValue(excessCapital)
 },[futureCashFlow])
   const calculatePresentValue=async (discountRate)=>
   {
     console.log(fcfEachyear)
+    let pvEachYearBreakDown=[];
       let presentvalue=0
+      let totalPresentValue=0
        fcfEachyear.forEach((value,index) => {
-        presentvalue+=(value/Math.pow((1+discountRate),(index+1)));
+        presentvalue=(value/Math.pow((1+discountRate),(index+1)));
+        totalPresentValue+=presentvalue;
+        console.log(presentvalue)
+        pvEachYearBreakDown.push(presentvalue)
       });
       let valueOfCompany=(tenthYearFCF/Math.pow((1+discountRate),10));
-      let futureCashFlowValue=valueOfCompany+presentvalue;
-
+      let futureCashFlowValue=valueOfCompany+totalPresentValue;
+      console.log("PV each year")
+      console.log(pvEachYearBreakDown)
+      console.log("*********")
       dispatch(
         {
           type:"updateFutureCashFlow",
           value:futureCashFlowValue
         })
+        dispatch(
+          {
+            type:"updatePresentValue",
+            value:pvEachYearBreakDown
+          })
+          dispatch(
+            {
+              type:"updateTenthYearPresentValue",
+              value:valueOfCompany
+            })
       return true;
   }
   const calculateIntrinsicValue= async (excessCapital)=>
@@ -118,39 +154,71 @@ useEffect(()=>{
       })
     return true;
   } 
+  useEffect(()=>{ 
+  if(marketCapCal !== 0)
+  dispatch(
+    {
+      type:"showHideResult",
+      value:true
+    })
+},[marketCapCal])
+const returnToValues=()=>
+{
+  dispatch(
+    {
+      type:"showHideResult",
+      value:false
+    })
+}
   return (
-    <div className="App">
+    
+    <div className="container">
+      
+   {(!showDecision || (showDecision === undefined))?
+    (<div className="valuesForCalculate componentContainer">
       <div>
-        <label htmlFor="firstFiveYearGR">Growth Rate (yrs 1 -5)</label>
+        <label htmlFor="firstFiveYearGR">Growth Rate (yrs 1 -5) {<Tooltip/>}</label>
         <input type="text" id="firstFiveYearGR" name="firstFiveYearGR" />
       </div>
       <div>
-        <label htmlFor="nextFiveYearGR">Growth rate (yrs 6 - 10)</label>
+        <label htmlFor="nextFiveYearGR">Growth rate (yrs 6 - 10) {<Tooltip/>}</label>
         <input type="text" id="nextFiveYearGR" name="nextFiveYearGR" />
       </div>
       <div>
-        <label htmlFor="terminalValue">Terminal Value <span id="terminalValueSelected"></span></label>
+        <label htmlFor="terminalValue">Terminal Value {<Tooltip/>} <span id="terminalValueSelected"></span></label>
         <input type="range" id="terminalValue" name="terminalValue" min="10" max="15" onChange={event => updateSelectedValue(event.target)}/>
       </div>
       <div>
-        <label htmlFor="discountValue">Discount Rate<span id="discountValueSelected"></span></label>
+        <label htmlFor="discountValue">Discount Rate {<Tooltip/>}<span id="discountValueSelected"></span></label>
         <input type="range" id="discountValue" name="discountValue" onChange={event => updateSelectedValue(event.target)} 
-          min="0" max="100" value="10"/>
+          min="0" max="100" />
       </div>
       <div>
-        <label htmlFor="freeCashFlow">Free Cash flow</label>
+        <label htmlFor="freeCashFlow">Free Cash flow {<Tooltip/>}</label>
         <input type="text" id="freeCashflow" name="freeCashflow" />
       </div>
       <div>
-        <label htmlFor="excessCapital">Excess Capital</label>
+        <label htmlFor="excessCapital">Excess Capital {<Tooltip/>}</label>
         <input type="text" id="excessCapital" name="excessCapital"  />
       </div>
       <div>
         <button id="canIBuyNow" name="canIBuyNow" onClick={calculateIntrinsic}>Can I buy now?</button>
       </div>
-      <div>
-        Calculated total Market cap is {marketCapCal}
-      </div>
+      </div>):
+      (<div className="componentContainer">
+          
+          <div>
+          Calculated total Market cap is {Math.round(marketCapCal)}
+        </div>
+          <Decision data={state} />
+          <div>
+            <button id="returnToCalculate" name="returnToCalculate" onClick={returnToValues}>Go Back</button>
+          </div>
+      </div>)
+      }
+      
+    
+    
     </div>
   );
 }
--- src/Decision.js
+++ src/Decision.js
@@ -1,5 +1,41 @@
-function Decision()
+function Decision(props)
 {
-    return <div>This is the decision component</div>
+    const generateColumns=(combinedValues)=>{
+        let columns=[]
+        if(combinedValue.fcfBreakDown.length===combinedValue.pvValue.length)
+        {
+            let numberofRows=combinedValue.fcfBreakDown.length;
+            for(let i=1;i<=numberofRows;i++)
+            {
+                columns.push(<tr><td>{i}</td>
+                          <td>{Math.round(combinedValue.fcfBreakDown[i-1])}</td>
+                          <td>{Math.round(combinedValue.pvValue[i-1])}</td></tr>)
+            }
+            columns.push(<tr><td>{numberofRows}</td>
+                <td>{Math.round(props.data.tenthYearFCF)}</td>
+                <td>{Math.round(props.data.tenthYearPV)}</td></tr>)
+        }
+        return columns;
+
+    }
+    console.log("Gonna render decision")
+    console.log(props.data)
+    let combinedValue=Object.assign({},{"fcfBreakDown":props.data.fcfEachyear,"pvValue":props.data.pvEachYear})
+    console.log(combinedValue)
+    return <div id="Decision">
+        <table>
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>FCF</th>
+                    <th>PV</th>
+                </tr>
+            </thead>
+            <tbody>
+                {generateColumns(combinedValue)}
+            </tbody>
+        </table>
+       
+    </div>
 }
 export default Decision;
